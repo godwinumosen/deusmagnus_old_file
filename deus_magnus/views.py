cec53558592a96cf8b5e075c8ef56c3d34621dc1
Godwin Umosen <umosengodwin568@gmail.com>
2024-04-17 04:40:29 -0700

    Main home page video file is been change to a construction video clip

--- deus_magnus/views.py
+++ deus_magnus/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView,ListView
+from django.contrib.auth.decorators import login_required
+from .models import DeusMagnusMainPost
+from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.urls import reverse_lazy
 
-# Create your views here.
+'''def home (request):
+    return render (request, 'deus_magnus/home.html')'''
 
-def home (request):
-    return render (request, 'deus_magnus/home.html')
+def base (request):
+    return render(request,"base.html")
+
+#The main HomeView page
+class HomeView(ListView):
+    model = DeusMagnusMainPost
+    template_name = 'deus_magnus/home.html'
+    
