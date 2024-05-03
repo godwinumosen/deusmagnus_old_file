f730ff72f43ce760f98a2c2d091cdd3e182e1c87
Godwin Umosen <umosengodwin568@gmail.com>
2024-05-03 07:57:11 -0700

    The zoon enable reactivated

--- deus_magnus/models.py
+++ deus_magnus/models.py
@@ -11,13 +11,13 @@ class DeusMagnusMainPost(models.Model):
     deus_magnus_description = models.TextField()
     deus_magnus_slug = models.SlugField (max_length=255,blank=True, null=True)
     deus_manus_video = models.FileField(upload_to='videos/')
-    #thumbnail = models.ImageField(max_length=100, null=True, blank=True)
+    thumbnail = models.ImageField(max_length=100, null=True, blank=True)
     deus_magnus_publish_date = models.DateTimeField (auto_now_add= True)
     deus_magnus_author = models.ForeignKey(User, on_delete=models.CASCADE)
 
        
     class Meta:
-        ordering =['deus_magnus_publish_date']
+        ordering =['-deus_magnus_publish_date']
     
     def __str__(self):
         return self.deus_magnus_title + ' | ' + str(self.deus_magnus_author)
