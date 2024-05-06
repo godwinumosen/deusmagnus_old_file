3aed5669bfea92cdde0c7a2b61efbbcad76ca538
Godwin Umosen <umosengodwin568@gmail.com>
2024-05-06 01:32:09 -0700

    Massive site upcoming

--- deus_magnus/views.py
+++ deus_magnus/views.py
@@ -48,7 +48,7 @@ class ArticleDetailView(DetailView):
     
 #The second ArticleDetailView page    
 class SecondConstructionDetailViewArticleDetailView(DetailView):
-    model = SecondDeusMagnusMainPicturePos
+    model = SecondDeusMagnusMainPicturePost
     template_name = 'deus_magnus/second_article_detail.html'
     context_object_name = 'second_construction'
 
