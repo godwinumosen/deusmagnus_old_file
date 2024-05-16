52789ddb03f67274a48ce478543d30794eabaaa0
Godwin Umosen <umosengodwin568@gmail.com>
2024-05-16 05:44:47 -0700

    information and presentation of the projects can be found here:

--- deus_magnus/views.py
+++ deus_magnus/views.py
@@ -30,7 +30,7 @@ class HomeView(ListView):
         context['last_constructions'] = LastDeusMagnusMainPicturePost.objects.all()   
         return context    
 
- #The first Deus Magnus Video ArticleDetailView page
+ #The first Deus Magnus Video ArticleDetailView pag
 class ArticleDetailView(DetailView):
     model = DeusMagnusMainPost
     template_name = 'deus_magnus/article_detail.html'
