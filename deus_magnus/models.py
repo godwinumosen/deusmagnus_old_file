6c31eaf7186af04c3891d8772202ba298de91f0a
Godwin Umosen <umosengodwin568@gmail.com>
2024-06-21 01:12:14 -0700

    ata-toggle = 'dropdown-toggle'

--- deus_magnus/models.py
+++ deus_magnus/models.py
@@ -30,7 +30,7 @@ class DeusMagnusMainPost(models.Model):
 class SecondDeusMagnusMainPicturePost(models.Model):
     second_deus_magnus_title = models.CharField(max_length=255, blank=True, null=True)
     second_deus_magnus_description = models.TextField()
-    second_deus_magnus_slug = models.SlugField (max_length=255,blank=True, null=True
+    second_deus_magnus_slug = models.SlugField (max_length=255,blank=True, null=True)
     second_deus_magnus_img = models.ImageField(upload_to='images/')
     second_deus_magnus_publish_date = models.DateTimeField (auto_now_add= True)
     second_deus_magnus_author = models.ForeignKey(User, on_delete=models.CASCADE)
