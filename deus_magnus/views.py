439162152b94c83807a31b008d1a76b41e5c9104
Godwin Umosen <umosengodwin568@gmail.com>
2024-06-24 04:31:31 -0700

    The last commit of the weekend

--- deus_magnus/views.py
+++ deus_magnus/views.py
@@ -50,7 +50,7 @@ class SecondConstructionDetailViewArticleDetailView(DetailView):
     context_object_name = 'second_construction'
 
     def SecondConstructionDetailViewArticleDetailView(request, pk)
-        object = get_object_or_404(SecondDeusMagnusMainPicturePost, pk=pk)
+        object = get_object_or_404(SecondDeusMagnusMainPicturePost, pk=pk
         return render(request, 'deus_magnus/second_article_detail.html', {'second_detail': object})
     #This sub-model data related to the second article model instance in deus magnus group
     def get_context_data(self, **kwargs):
