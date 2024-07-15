715f705b000d07ec2c86e3e04fa7dcf05aa098f9
Godwin Umosen <umosengodwin568@gmail.com>
2024-07-15 09:03:17 -0700

    the commit fot]]r the about page of the web app

--- deus_magnus/admin.py
+++ deus_magnus/admin.py
@@ -4,13 +4,13 @@ from . import models
 from .models import DeusMagnusMainPost, SecondDeusMagnusMainPicturePost, LastDeusMagnusMainPicturePost,SubPicture_2
 from .models import SubPicture_1, VideoSubImage, BlogDeusMagnus,BoardOfDirectorsInDeusMagnus,OurManagementsInDeusMagnus
 
-#The DeusMagnus post post model admin of josepdam
+#The DeusMagnus post,post model admin
 class DeusMagnusMainPostModelAdmin (admin.ModelAdmin):
     prepopulated_fields = {'deus_magnus_slug': ('deus_magnus_title',)}
     list_display = ['deus_magnus_title','deus_magnus_description','deus_manus_video','deus_magnus_author']
 admin.site.register(DeusMagnusMainPost, DeusMagnusMainPostModelAdmin)
 
-#The Secon DeusMagnus post post model admin of josepdam
+#The Second DeusMagnus post post model admin of josepdam
 class SecondDeusMagnusMainPicturePosttModelAdmin (admin.ModelAdmin):
     prepopulated_fields = {'second_deus_magnus_slug': ('second_deus_magnus_title',)}
     list_display = ['second_deus_magnus_title','second_deus_magnus_description','second_deus_magnus_img','second_deus_magnus_author']
