e6726df6b547f87e2f6f7d2da4830eff506a88c7
Godwin Umosen <umosengodwin568@gmail.com>
2024-07-22 09:02:58 -0700

    header is been taking a font-family of a Merriweather

--- deus_magnus/views.py
+++ deus_magnus/views.py
@@ -103,7 +103,7 @@ class SubVideoDetailView(DetailView):
     context_object_name = 'sub_video'
     def SubVideoDetailView(request, pk):  
         object = get_object_or_404(SubVideoDetailView, pk=pk)
-        return render(request, 'deus_magnus/sub_video_detail.html', {'sub_detail_video':object})    
+        return render(request, 'deus_magnus/sub_video_detail.html', {'sub_detail_video':object}) 
 
 #This category is for the Whatsapp API for deus magnus
 def deus_magnus_whatsapp_message(request):
@@ -111,7 +111,6 @@ def deus_magnus_whatsapp_message(request):
     dues_magnus_whatsapp_link = f'https://api.whatsapp.com/send?phone={deus_magnus_whatsapp_number}'
     context = {'whatsapp_link': dues_magnus_whatsapp_link}
     return render(request, 'deus_magnus_kwhatsapp_message.html', context)
-
 
 # The Contact view been implemented
 def ContactView (request):
