890d91fb67dd0d9990ca2ad2e08747e4c39f51db
Godwin Umosen <umosengodwin568@gmail.com>
2024-08-07 03:12:47 -0700

    Premium Facility Management

--- deus/urls.py
+++ deus/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path ('',include ('deus_magnus.urls')),
 ]
 
+admin.site.site_header = "Deusmagnus Admin "
+admin.site.site_title = " Deusmagnus Ltd "
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
