bb1a446fe39e55b823cc98d21bfb532432d12bf1
Godwin Umosen <umosengodwin568@gmail.com>
2024-08-23 07:25:42 -0700

    The site last commit

--- deus_magnus/models.py
+++ deus_magnus/models.py
@@ -6,12 +6,7 @@ from datetime import datetime, date
 #The first main video on deusmagnus website
 class Mainvideo(models.Model):
     deus_magnus_first_video = models.FileField(upload_to='main_videos/') 
-#the footer description
-class FooterPost(models.Model):
-    footer_title = models.CharField(max_length=100, blank=True, null=True)
-    # other fields as needed
-    def __str__(self):
-        return self.footer_title
+
 # The main model for Deus Magnus Model category
 class DeusMagnusMainPost(models.Model):
     deus_magnus_title = models.CharField(max_length=255, blank=True, null=True)
@@ -223,3 +218,11 @@ class GLOSSARY(models.Model):
     
     def get_absolute_url(self):
         return reverse('home')
+
+
+#the footer description
+class FooterPost(models.Model):
+    footer_title = models.CharField(max_length=100, blank=True, null=True)
+    # other fields as needed
+    def __str__(self):
+        return self.footer_title
