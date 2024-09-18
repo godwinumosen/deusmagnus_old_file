b671bfc9cfac55c99d0b0d234325b4f20d342800
Godwin Umosen <umosengodwin568@gmail.com>
2024-09-18 07:17:28 -0700

    All implementations of the site is accurate

--- deus_magnus/views.py
+++ deus_magnus/views.py
@@ -64,7 +64,7 @@ class SecondConstructionDetailViewArticleDetailView(DetailView):
         return render(request, 'deus_magnus/second_article_detail.htm', {'second_detail': object})
     #This sub-model data related to the second article model instance in deus magnus group
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs
         context['subs'] = SubPicture_1.objects.all() 
         return context
 
