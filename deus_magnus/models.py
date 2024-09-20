39bb300268704ec715e64ce27ebb49077559f23a
Godwin Umosen <umosengodwin568@gmail.com>
2024-09-20 08:59:07 -0700

    The window view is activated

--- deus_magnus/models.py
+++ deus_magnus/models.py
@@ -12,7 +12,7 @@ class DeusMagnusMainPost(models.Model):
     deus_magnus_title = models.CharField(max_length=255, blank=True, null=True)
     deus_magnus_description = models.TextField()
     deus_magnus_slug = models.SlugField (max_length=255,blank=True, null=True)
-    deus_manus_video = models.FileField(upload_to='videos/'
+    deus_manus_video = models.FileField(upload_to='videos/') 
     #thumbnail = models.ImageField(max_length=100, null=True, blank=True)
     deus_magnus_publish_date = models.DateTimeField (auto_now_add= True)
     deus_magnus_author = models.ForeignKey(User, on_delete=models.CASCADE)
