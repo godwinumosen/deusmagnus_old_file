48b2eb2c79496176c528a4dff245a867e203b219
Godwin Umosen <umosengodwin568@gmail.com>
2024-09-27 07:53:45 -0700

    The guides py is been added to the app

--- deus_magnus/models.py
+++ deus_magnus/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+
 from datetime import datetime, date
 
 #The first main video on deusmagnus website
