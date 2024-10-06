eda11f9e62e6f45cd214dd9eb48c59c3a5051a44
Godwin Umosen <umosengodwin568@gmail.com>
2024-10-06 12:51:37 -0700

    New articles of all pages updated

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,42 +1,53 @@
-	$(".js-height-full").height($(window).height());
-	$(".js-height-parent").each(function() {
-	    $(this).height($(this).parent().first().height());
-	});
+$(document).ready(function() {
+    $(".js-height-full").height($(window).height());
+    $(".js-height-parent").each(function() {
+        $(this).height($(this).parent().first().height());
+    });
 
+    // Fun Facts
+    function count($this) {
+        var current = parseInt($this.data('current'), 10); // Get the current count from data attribute
+        current += 1; // Increment value
 
-	// Fun Facts
-	function count($this) {
-	    var current = parseInt($this.html(), 10);
-	    current = current + 1; /* Where 50 is increment */
+        // Update only the numeric part while keeping the + sign
+        $this.html(current + ' <span>+</span>');
 
-	    $this.html(++current);
-	    if (current > $this.data('count')) {
-	        $this.html($this.data('count'));
-	    } else {
-	        setTimeout(function() {
-	            count($this)
-	        }, 50);
-	    }
-	}
+        // Store the updated current value
+        $this.data('current', current); // Update data-current
 
-	$(".stat-timer").each(function() {
-	    $(this).data('count', parseInt($(this).html(), 10));
-	    $(this).html('0');
-	    count($(this));
-	});
+        if (current < $this.data('count')) {
+            setTimeout(function() {
+                count($this);
+            }, 50);
+        } else {
+            $this.html($this.data('count') + ' <span class="plus">+</span>'); // Final value
+        }
+    }
 
+    $(".stat-timer").each(function() {
+        var countTo = parseInt($(this).text(), 10); // Get the initial count
+        $(this).data('count', countTo); // Set the target count
+        $(this).data('current', 0); // Initialize current count
 
+        // Start counting only if the countTo is greater than 0
+        if (countTo > 0) {
+            count($(this)); // Start the counting
+        } else {
+            $(this).html(countTo + ' <span>+</span>'); // If it's 0 or not a number, set it directly
+        }
+    });
 
-	$('.header').affix({
-	    offset: {
-	        top: 100,
-	        bottom: function() {
-	            return (this.bottom = $('.footer').outerHeight(true))
-	        }
-	    }
-	})
+    $('.header').affix({
+        offset: {
+            top: 100,
+            bottom: function() {
+                return (this.bottom = $('.footer').outerHeight(true));
+            }
+        }
+    });
 
-	$(window).load(function() {
-	    $("#preloader").on(500).fadeOut();
-	    $(".preloader").on(600).fadeOut("slow");
-	});
+    $(window).on('load', function() {
+        $("#preloader").delay(500).fadeOut();
+        $(".preloader").delay(600).fadeOut("slow");
+    });
+});
