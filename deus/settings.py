18d8f04c5bcf4fef1a68e2ed3758c82b36166118
Godwin Umosen <umosengodwin568@gmail.com>
2024-10-12 08:00:07 -0700

    New articles of all pages updated

--- deus/settings.py
+++ deus/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'deus_magnus'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
