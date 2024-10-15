47a86bd9d3ebd4394ea07e2256996956990e8354
Godwin Umosen <umosengodwin568@gmail.com>
2024-10-15 06:17:13 -0700

    1*************

--- deus/settings.py
+++ deus/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'deus.urls'
 
 TEMPLATES = [
